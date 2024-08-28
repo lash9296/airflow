c83c5f19602ff923013f2f42c581e6ef445d97a2
Lash <tjrrjwu92@gmail.com>
2024-08-28 21:06:14 +0900

    Refactor and add new dags files

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -3,7 +3,7 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_bash_operator", #테스트중
     schedule="30 2 * * *",  # 매일 02:30에 실행
     start_date=pendulum.datetime(2024, 8, 12, 2, 35, tz="Asia/Seoul"),  # 2024년 8월 12일 02:30 시작
     catchup=False,  # 과거 실행을 무시
