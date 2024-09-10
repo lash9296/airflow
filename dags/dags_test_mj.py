6334a5a4b747648dd61a24a8a446e3d8e69b42f3
Lash <tjrrjwu92@gmail.com>
2024-09-10 15:49:22 +0900

    24-09-10 test

---
+++ dags/dags_test_mj.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_operator", #테스트중 맞나?
+    schedule="30 2 * * *",  # 매일 02:30에 실행
+    start_date=pendulum.datetime(2024, 8, 12, 2, 35, tz="Asia/Seoul"),  # 2024년 8월 12일 02:30 시작
+    catchup=False,  # 과거 실행을 무시
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    # 실행 순서
+    bash_t1 >> bash_t2
