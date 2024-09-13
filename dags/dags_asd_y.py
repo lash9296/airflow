a70523bd35caec2406e4fcb264de7fa85bb9ee73
Lash <tjrrjwu92@gmail.com>
2024-09-13 13:43:19 +0900

    '완료'

--- dags/dags_asd_y.py
+++ dags/dags_asd_y.py
@@ -16,31 +16,5 @@ with DAG(
     t2 = EmptyOperator(
         task_id = 't2'
     )
+    t1 >> t2
     
-    t3 = EmptyOperator(
-        task_id = 't3'
-    )
-    
-    t4 = EmptyOperator(
-        task_id = 't4'
-    )
-    
-    t5 = EmptyOperator(
-        task_id = 't5'
-    )
-    
-    t6 = EmptyOperator(
-        task_id = 't6'
-    )
-        
-    t7 = EmptyOperator(
-        task_id = 't7'
-    )
-        
-    t8 = EmptyOperator(
-        task_id = 't8'
-    )
-    
-    t1 >> [t2, t3] >> t4
-    t5 >> t4
-    [t4, t7] >> t6 >> t8
