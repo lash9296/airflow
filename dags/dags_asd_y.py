0be6cf869226d872f0ba36c183cf62d72eb3f001
Lash <tjrrjwu92@gmail.com>
2024-09-17 18:30:27 +0900

    테스트

--- dags/dags_asd_y.py
+++ dags/dags_asd_y.py
@@ -6,7 +6,7 @@ from airflow.operators.empty import EmptyOperator
 with DAG(
     dag_id="dags_conn_test",
     start_date=pendulum.datetime(2024, 8, 12, 21, 53, tz="Asia/Seoul"),  # 2024년 8월 12일 02:30 시작
-    catchup=False,  # 과거 실행을 무시 조민준
+    catchup=False,  # 과거 실행을 무시 조민준123
 ) as dag:
     
     t1 = EmptyOperator(
@@ -16,5 +16,5 @@ with DAG(
     t2 = EmptyOperator(
         task_id = 't2'
     )
-    t1 >> t2
+    t1 >> t2 >> t1
     
